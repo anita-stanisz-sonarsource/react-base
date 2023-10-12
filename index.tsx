181c5d8468d0df2918a7dc82c11f28323cab1cc7
Shane Findley <shane.findley@sonarsource.com>
2023-10-12 16:14:39 +0200

    Forced failure

--- index.tsx
+++ index.tsx
@@ -5,18 +5,6 @@ import App from './src/app';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement!);
 
-const s3 = require('aws-cdk-lib/aws-s3');
-
-new s3.Bucket(this, 'id', {
-    bucketName: 'bucket'
-}); // Sensitive
-
-const s32 = require('aws-cdk-lib/aws-s3');
-
-new s32.Bucket(this, 'id', {
-    bucketName: 'bucket'
-}); // Sensitive
-
 root.render(
     <StrictMode>
         <App />
