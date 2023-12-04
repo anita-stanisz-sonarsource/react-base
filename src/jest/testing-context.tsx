06d4c19e7fd05c978d5c6ae423da2c118888c9ad
Shane Findley <shane.findley@sonarsource.com>
2023-12-04 09:17:20 +0100

    Update testing-context.tsx

--- src/jest/testing-context.tsx
+++ src/jest/testing-context.tsx
@@ -1,20 +1,4 @@
 import { createContext } from 'react'
-import { createCipheriv, createDecipheriv } from 'crypto';
-
-const encryption_key = "byz9VFNtbRQM0yBODcCb1lrUtVVH3D3x"; // Must be 32 characters
-const initialization_vector = "X05IGQ5qdBnIqAWD"; // Must be 16 characters
-function encrypt(text){
-  const cipher = createCipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.from(initialization_vector))
-  let crypted = cipher.update(text, 'utf8', 'hex')
-  crypted += cipher.final('hex')
-  return crypted  
-}
-function decrypt(text){ 
-  const decipher = createDecipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.from(initialization_vector))
-  let dec = decipher.update(text, 'hex', 'utf8')
-  dec += decipher.final('utf8')
-  return dec 
-} 
 
 export interface ComponentContextShape {
   value: string | undefined
@@ -22,6 +6,7 @@ export interface ComponentContextShape {
 }
 
 export function mapToMessages() {
+  var blah = 'test';
   console.log("blah");
   console.log("blah");
   console.log("blah");
