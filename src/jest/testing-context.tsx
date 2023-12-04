a27ac5365ae7d6d6351ef50445e8245151b6b6a3
Shane Findley <shane.findley@sonarsource.com>
2023-12-04 08:46:19 +0100

    Update testing-context.tsx

--- src/jest/testing-context.tsx
+++ src/jest/testing-context.tsx
@@ -1,4 +1,21 @@
 import { createContext } from 'react'
+import { createCipheriv, createDecipheriv } from 'crypto';
+
+const encryption_key = "byz9VFNtbRQM0yBODcCb1lrUtVVH3D3x"; // Must be 32 characters
+const initialization_vector = "X05IGQ5qdBnIqAWD"; // Must be 16 characters
+function encrypt(text){
+  const cipher = createCipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.from(initialization_vector))
+Use a secure mode and padding scheme.
+  var crypted = cipher.update(text, 'utf8', 'hex')
+  crypted += cipher.final('hex')
+  return crypted  
+}
+function decrypt(text){ 
+  const decipher = createDecipheriv('aes-256-cbc',Buffer.from(encryption_key), Buffer.from(initialization_vector))
+  let dec = decipher.update(text, 'hex', 'utf8')
+  dec += decipher.final('utf8')
+  return dec 
+} 
 
 export interface ComponentContextShape {
   value: string | undefined
