7a329930fe56c919d3fd21521cbb415f1fb0ff08
Shane Findley <shane.findley@sonarsource.com>
2023-12-22 09:06:05 +0100

    Update testing-component.tsx

--- src/jest/testing-component.tsx
+++ src/jest/testing-component.tsx
@@ -1,4 +1,50 @@
 import { useState } from 'react'
+  
+export const timeFormat = 'HH:mm:ss'
+export const timeFormat2 = 'HH:mm:ss'
+export const timeFormat3 = 'HH:mm:ss' 
+export const timeFormat4 = 'HH:mm:ss'
+export const timeFormat5 = 'HH:mm:ss'
+
+export function echo() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+} 
+
+export function echo2() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo3() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo4() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo5() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
 
 const STATUS = {
   HOVERED: 'hovered',
