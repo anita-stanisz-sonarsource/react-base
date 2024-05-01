2439c0702c82e640a3c68f1d25da642cb452e675
Anita Stanisz <anita.stanisz@sonarsource.com>
2024-05-01 14:43:49 +0200

    introducing issues

--- src/jest/testing-component.tsx
+++ src/jest/testing-component.tsx
@@ -1,5 +1,51 @@
 import { useState } from 'react'
 
+export const timeFormat = 'HH:mm:ss'
+export const timeFormat2 = 'HH:mm:ss'
+export const timeFormat3 = 'HH:mm:ss'
+export const timeFormat4 = 'HH:mm:ss'
+export const timeFormat5 = 'HH:mm:ss'
+
+export function echo() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo2() {
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+  console.log();
+}
+
+export function echo3() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo4() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
+export function echo5() {
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+  console.error();
+}
+
 const STATUS = {
   HOVERED: 'hovered',
   NORMAL: 'normal'
